61957733fce3dd649472d705b769c10b68305e1b
Shantnu Tiwari <shantnu@Shantnus-MacBook-Air.local>
2024-05-24 12:19:57 +0100

    working with auth

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-
+import pdb
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -18,7 +18,7 @@ user_allowed_countries = {
 
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(password, users[username]):
+    if username in users and check_password_hash(users.get(username), password):
         return username
 
 @auth.error_handler
@@ -29,6 +29,27 @@ def unauthorized():
 @auth.login_required
 def home():
     return jsonify({"message": "Hello world"})
+
+@app.route("/rest/<method>", methods = ['GET'])
+@auth.login_required
+def rest_api_data(method):
+    country_code = request.args.get('countryCode')
+    language_code = request.args.get('languageCode')
+    search_text = request.args.get('searchText')
+
+    if auth.current_user() is None:
+        return jsonify({"message": "UNAuthorised User"}), 403
+    if not country_code:
+        return jsonify({"message": "Missing Country code!"}), 400
+    if country_code == "RU":
+        return jsonify({"message": "Sorry, Access to Russia is forbidden!"}), 403
+
+    # check user has permissions for country
+    if country_code not in user_allowed_countries[auth.current_user()]:
+        return jsonify({"message": "Sorry, user doesnt have permissions to access this country"}), 403
+
+    return_message = f"The server found this message {search_text} for country {country_code} {language_code=}"
+    return jsonify({"message": return_message}),200
 
 
 if __name__ == '__main__':
--- test_server.py
+++ test_server.py
@@ -1,8 +1,40 @@
 import requests
+from requests.auth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash
 
-TEST_URL = "http://127.0.0.1:5000"
+TEST_URL = "http://127.0.0.1:5000/rest/List"
+params = {
+    'countryCode': 'IN',
+    'languageCode': 'en',
+    'searchText': 'hello india'
+}
 
-response = requests.get(TEST_URL)
+# Define the username and password
+username = 'user2'
+password = 'password2'
+
+
+
+# Make the GET request with basic authentication
+response = requests.get(TEST_URL, params=params, auth=HTTPBasicAuth(username, password))
+
+print('Status Code:', response.status_code)
+print('Response JSON:', response.json())
+
+params = {
+    'countryCode': 'DE',
+    'languageCode': 'de',
+    'searchText': 'Hallo Deutschland über Straße'
+}
+
+# Define the username and password
+username = 'user1'
+password = 'password1'
+
+# hashed_password = generate_password_hash(password)
+
+# Make the GET request with basic authentication
+response = requests.get(TEST_URL, params=params, auth=HTTPBasicAuth(username, password))
 
 print('Status Code:', response.status_code)
 print('Response JSON:', response.json())
